c48df85ab377f0eee9b61063d86054f8b940c5fc
谭云锴 <2472746777@qq.com>
2020-05-23 17:56:46 +0800

    张三的修改

---
+++ mine-git/src/zhangsan.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class zhangsan extends Component {
+    render() {
+        return (
+            <div>
+                我是张三啊
+            </div>
+        )
+    }
+}
