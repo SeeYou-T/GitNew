f366d3fd4e494f679de8f0097146bdb98a163d95
谭云锴 <2472746777@qq.com>
2020-05-25 21:47:09 +0800

    developer

---
+++ mine-git/src/developer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class developer extends Component {
+    render() {
+        return (
+            <div>
+                developer
+            </div>
+        )
+    }
+}
